c406f341db5b824b03eebb0e74bb95b1e4ebe33a
Daniel Roberto <sdonatordaniel@gmail.com>
2022-11-29 20:53:11 -0300

    fix: remove password from data json

--- src/server/controllers/authController.ts
+++ src/server/controllers/authController.ts
@@ -9,6 +9,7 @@ export default class UserController {
         return res.status(400).send({ error: "User already exists" });
       }
       const user = await User.create(req.body);
+      user.password = undefined;
       return res.send({ user });
     } catch (err) {
       return res.status(400).send({ error: "Registration failed" });
